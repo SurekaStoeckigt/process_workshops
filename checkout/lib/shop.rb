3381f7a049d22dccb9ef7e6983192c8c2c90209c
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-04-28 13:23:09 +0100

    added checkout test

--- checkout/lib/shop.rb
+++ checkout/lib/shop.rb
@@ -1,11 +1,23 @@
 class Shop
+ALL_ITEMS = {A: 50, B: 30, C: 20, D: 15}
 
-ALL_ITEMS = {"A" => 150, "B" => 30, "C" => 20, "D" => 15}
+  def initialize
+    @price = 0
+  end
 
   def checkout(item)
-    item.chars.each do |letter|
-      return -1 unless ALL_ITEMS.key?(letter)
-    end 
+    return -1 if item.is_a?Integer
+    item.each_char do |letter|
+      return -1 unless ALL_ITEMS.key?(letter.to_sym)
+    end
+    calc_price(item)
+  end
+
+  def calc_price(item)
+    item.chars.each do |char|
+       @price += ALL_ITEMS[char.to_sym]
+    end
+    return @price
   end
 
 end
--- checkout/spec/shop_spec.rb
+++ checkout/spec/shop_spec.rb
@@ -12,4 +12,14 @@ describe Shop do
     expect(shop.checkout("-B8x")).to eq(-1)
   end
 
+  it "returns -1 for 18" do
+    shop = Shop.new
+    expect(shop.checkout(18)).to eq(-1)
+  end
+
+  it "returns 100 for AA" do
+    shop = Shop.new
+    expect(shop.checkout("AA")).to eq(100)
+  end
+
 end
